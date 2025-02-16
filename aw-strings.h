8699bbc9884ee4db9c706eeb87fadd9d2d851375
afterwise <malte@hapki.se>
2025-02-16 09:44:20 +0100

    Another strings revamp

--- aw-strings.h
+++ aw-strings.h
@@ -24,8 +24,6 @@
 #ifndef AW_STRINGS_H
 #define AW_STRINGS_H
 
-#define __STDC_WANT_LIB_EXT1__ 1
-
 #include <stdarg.h>
 #if !defined(_MSC_VER) || _MSC_VER >= 1800
 # include <stdbool.h>
@@ -66,63 +64,13 @@ typedef signed __int64 strings_ssize_t;
 typedef ssize_t strings_ssize_t;
 #endif
 
+_strings_alwaysinline
+static int _vstrscanf(const char *__restrict str, const char *__restrict format, va_list ap) {
 #if defined(__GNUC__)
-
-_strings_alwaysinline
-static int _vstrscanf(const char *__restrict str, const char *__restrict format, va_list ap) {
 	return vsscanf(str, format, ap);
-}
-
-_strings_scanformat(2, 3)
-_strings_unused
-static inline int _strscanf(const char *__restrict str, const char *__restrict format, ...) {
-	va_list ap;
-	va_start(ap, format);
-	int err = _vstrscanf(str, format, ap);
-	va_end(ap);
-	return err;
-}
-
-_strings_alwaysinline
-static int _vstrnprintf(char *__restrict str, size_t size, const char *__restrict format, va_list ap) {
-	return vsnprintf(str, size, format, ap);
-}
-
-_strings_printformat(3, 4)
-_strings_unused
-static inline int _strnprintf(char *__restrict str, size_t size, const char *__restrict format, ...) {
-	va_list ap;
-	va_start(ap, format);
-	int err = _vstrnprintf(str, size, format, ap);
-	va_end(ap);
-	return err;
-}
-
-_strings_alwaysinline
-static bool _strcpy(char *dst, size_t dstsize, const char *src) {
-	return strcpy_s(dst, dstsize, src) == 0;
-}
-
-_strings_alwaysinline
-static int _strcasecmp(const char *s1, const char *s2) {
-	return strcasecmp(s1, s2);
-}
-
-_strings_alwaysinline
-static int _strncasecmp(const char *s1, const char *s2, size_t n) {
-	return strncasecmp(s1, s2, n);
-}
-
-_strings_alwaysinline
-static const char *_strcasestr(const char *haystack, const char *needle) {
-	return strcasestr(haystack, needle);
-}
-
 #elif defined(_MSC_VER)
-
-_strings_alwaysinline
-static int _vstrscanf(const char *__restrict str, const char *__restrict format, va_list ap) {
 	return vsscanf_s(str, format, ap);
+#endif
 }
 
 _strings_scanformat(2, 3)
@@ -137,7 +85,11 @@ static inline int _strscanf(const char *__restrict str, const char *__restrict f
 
 _strings_alwaysinline
 static int _vstrnprintf(char *__restrict str, size_t size, const char *__restrict format, va_list ap) {
+#if defined(__GNUC__)
+	return vsnprintf(str, size, format, ap);
+#elif defined(_MSC_VER)
 	return vsnprintf_s(str, size, _TRUNCATE, format, ap);
+#endif
 }
 
 _strings_printformat(3, 4)
@@ -151,31 +103,54 @@ static inline int _strnprintf(char *__restrict str, size_t size, const char *__r
 }
 
 _strings_alwaysinline
+_strings_unused
 static bool _strcpy(char *dst, size_t dstsize, const char *src) {
+	if (dst == nullptr || dstsize == 0 || src == nullptr)
+		return false;
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+	return strlcpy(dst, src, dstsize) < dstsize;
+#elif defined(__GNUC__)
+	strncpy(dst, src, dstsize);
+	if (dst[dstsize - 1] == 0)
+		return true;
+	dst[dstsize - 1] = 0;
+	return false;
+#elif defined(_MSC_VER)
 	return strcpy_s(dst, dstsize, src) == 0;
+#endif
 }
 
 _strings_alwaysinline
 static int _strcasecmp(const char *s1, const char *s2) {
+#if defined(__GNUC__)
+	return strcasecmp(s1, s2);
+#elif defined(_MSC_VER)
 	return _stricmp(s1, s2);
+#endif
 }
 
 _strings_alwaysinline
 static int _strncasecmp(const char *s1, const char *s2, size_t n) {
+#if defined(__GNUC__)
+	return strncasecmp(s1, s2, n);
+#elif defined(_MSC_VER)
 	return _strnicmp(s1, s2, n);
+#endif
 }
 
 _strings_alwaysinline
 static const char *_strcasestr(const char *haystack, const char *needle) {
+#if defined(__GNUC__)
+	return strcasestr(haystack, needle);
+#elif defined(_MSC_VER)
 	strings_ssize_t h = (strings_ssize_t) (haystack ? strlen(haystack) : 0);
 	strings_ssize_t n = (strings_ssize_t) (needle ? strlen(needle) : 0);
 	for (; h >= n; --h, ++haystack)
 		if (_strncasecmp(haystack, needle, n) == 0)
 			return haystack;
 	return NULL;
+#endif
 }
-
-#endif // defined(_MSC_VER)
 
 #ifdef __cplusplus
 } /* extern "C" */
